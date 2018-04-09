f019c90078d72a19c043d22cd67dc7872cfdf38d
Lingkai.Xu <xlk910728@gmail.com>
2018-04-09 16:33:29 +1000

    Add customized post type: auart (article from auliving)

--- ex-post.php
+++ ex-post.php
@@ -7,7 +7,7 @@ function my_custom_post_goodman() {
   );
   $args = array(
     'labels'        => $labels,
-    'description'   => '我们网站的电影信息',
+    'description'   => '明星商家',
     'public'        => true,
     'menu_position' => 5,
     'supports'      => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'custom-fields' ),
@@ -17,3 +17,21 @@ function my_custom_post_goodman() {
   register_post_type( 'goodman', $args );
 }
 add_action( 'init', 'my_custom_post_goodman' );
+function my_custom_post_auart() {
+  $labels = array(
+    'name'                  => _x( 'Auarts', 'Post Type General Name', 'text_domain' ),
+    'singular_name'         => _x( 'Auart', 'Post Type Singular Name', 'text_domain' ),
+    'menu_name'          => '热门文章'
+  );
+  $args = array(
+    'labels'        => $labels,
+    'description'   => '生活网热门文章',
+    'public'        => true,
+    'menu_position' => 5,
+    'supports'      => array( 'title',  'author', 'thumbnail', 'custom-fields' ),
+    'menu_icon'     => 'dashicons-money',
+    'has_archive'   => false
+  );
+  register_post_type( 'auart', $args );
+}
+add_action( 'init', 'my_custom_post_auart' );
